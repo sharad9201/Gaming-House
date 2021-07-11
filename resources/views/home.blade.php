3f2c75945e616854ea033c0b2d6a86cafc01c89b
sharad9201 <sharad.rae19@gmail.com>
2021-07-11 16:16:42 +0545

    auth views inside

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <p><a href="/games">View Your Orders</a></p>
                 </div>
             </div>
         </div>
